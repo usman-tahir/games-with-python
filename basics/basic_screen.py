ad9f82238ace519a94c437be078fecba127eac70
usman-tahir <tahir.usman.ali94@outlook.com>
2017-07-31 21:28:39 -0400

    made the basic screen example fully functional, with a game timer

--- basics/basic_screen.py
+++ basics/basic_screen.py
@@ -1,16 +1,51 @@
+
+from __future__ import print_function, division
 import pygame
+
+# Initialize Pygame.
 pygame.init()
+# Set size of pygame window.
+screen = pygame.display.set_mode((640,480))
+# Create empty pygame surface.
+background = pygame.Surface(screen.get_size())
+# Fill the background white color.
+background.fill((255, 255, 255))
+# Convert Surface object to make blitting faster.
+background = background.convert()
+# Copy background to screen (position (0, 0) is upper left corner).
+screen.blit(background, (0,0))
+# Create Pygame clock object.
+clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((640,480)) # Set screen size of pygame window
-background = pygame.Surface(screen.get_size())  # Create empty pygame surface
-background.fill((255,255,255))     # Fill the background white color (red,green,blue)
-background = background.convert()  # Convert Surface to make blitting faster
+mainloop = True
+# Desired framerate in frames per second. Try out other values.
+FPS = 30
+# How many seconds the "game" is played.
+playtime = 0.0
 
-screen.blit(background, (0, 0))
+while mainloop:
+    # Do not go faster than this framerate.
+    milliseconds = clock.tick(FPS)
+    playtime += milliseconds / 1000.0
 
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        mainloop = False # pygame window closed by user
-    elif event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_ESCAPE:
-            mainloop = False # user pressed ESC
+    for event in pygame.event.get():
+        # User presses QUIT-button.
+        if event.type == pygame.QUIT:
+            mainloop = False
+        elif event.type == pygame.KEYDOWN:
+            # User presses ESCAPE-Key
+            if event.key == pygame.K_ESCAPE:
+                mainloop = False
+
+    # Print framerate and playtime in titlebar.
+    text = "FPS: {0:.2f}   Playtime: {1:.2f}".format(clock.get_fps(), playtime)
+    pygame.display.set_caption(text)
+
+    #Update Pygame display.
+    pygame.display.flip()
+
+# Finish Pygame.
+pygame.quit()
+
+# At the very last:
+print("This game was played for {0:.2f} seconds".format(playtime))
